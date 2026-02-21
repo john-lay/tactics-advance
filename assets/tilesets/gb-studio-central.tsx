6c071245b49a99873ce321363508d32e771290f8
john-lay <john_lay@hotmail.co.uk>
2026-02-21 18:56:51 +0000

    Experiment with quantization and tilemaps

---
+++ assets/tilesets/gb-studio-central.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="gb-studio-central" tilewidth="8" tileheight="8" tilecount="1440" columns="40">
+ <image source="gb-studio-central.png" width="320" height="288"/>
+</tileset>
